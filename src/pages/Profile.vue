a575511dfc317296b79a1ae19f7944bf29033eee
iLikeTree <1220862922@qq.com>
2017-03-07 23:32:06 +0800

    update 我的

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -18,6 +18,67 @@
                     </div>
                 </div>
             </div>
+            <div class="profile-notice">
+                <div class="profile-notice-tab flex">
+                    <img class="icon-img" src="../assets/images/ic_mine_notification.png">
+                    <p class="flex-fit">提醒</p>
+                    <img class="icon-img icon-right" src="../assets/images/ic_mine_notification.png">
+                </div>
+                <p class="profile-notice-tip">暂无提醒</p>
+            </div>
+            <div class="profile-tab">
+                <ul class="profile-tab-list flex">
+                    <li>
+                        <img class="icon-img" src="../assets/images/ic_my_likes.png">
+                        喜欢
+                    </li>
+                    <li>
+                        <img class="icon-img" src="../assets/images/ic_my_note.png">
+                        日记
+                    </li>
+                    <li>
+                        <img class="icon-img" src="../assets/images/ic_my_album.png">
+                        相册
+                    </li>
+                    <li>
+                        <img class="icon-img" src="../assets/images/ic_my_status.png">
+                        我的广播
+                    </li>
+                </ul>
+                <ul class="profile-tab-list flex">
+                    <li>
+                        <img class="icon-img" src="../assets/images/ic_my_movies_tvs.png">
+                        电影·电视
+                    </li>
+                    <li>
+                        <img class="icon-img" src="../assets/images/ic_my_books.png">
+                        读书
+                    </li>
+                    <li>
+                        <img class="icon-img" src="../assets/images/ic_my_music.png">
+                        音乐
+                    </li>
+                    <li>
+                        <img class="icon-img" src="../assets/images/ic_my_events.png">
+                        同城活动
+                    </li>
+                </ul>
+                <ul class="profile-tab-list flex">
+                    <li>
+                        <img class="icon-img" src="../assets/images/ic_my_doulist.png">
+                        豆列
+                    </li>
+                    <li>
+                        <img class="icon-img" src="../assets/images/ic_my_orders.png">
+                        订单
+                    </li>
+                    <li>
+                        <img class="icon-img" src="../assets/images/ic_my_wallet.png">
+                        钱包
+                    </li>
+                </ul>
+            </div>
+
         </div>
     </div>
 </template>

679050dd30df8ff54f8c14d29790a93ad2fdc48b
iLikeTree <1220862922@qq.com>
2017-03-20 23:43:03 +0800

    update

--- src/pages/Subject.vue
+++ src/pages/Subject.vue
@@ -22,16 +22,23 @@
                             <li v-for="(item, index) in hotMovies.subjects" v-if="index < 10" class="movie-item">
                                 <a href="javascript:void(0);">
                                     <div class="movie-poster" :style="'background-image:url(' + item.images.large+ ')'"></div>
-                                    <p>{{ item.title }}</p>
+                                    <p class="movie-item-name">{{ item.title }}</p>
+                                    <div class="movie-item-rating">
+                                        <div class="rating-star-bg">
+                                            <span class="rating-star-on" :style="'width:'+ 10*item.rating.average +'%'"></span>
+                                        </div>
+                                        <span>{{item.rating.average}}</span>
+                                    </div>
                                 </a>
                             </li>
                             <li class="movie-item">
                                 <a href="javascript:void(0);">
                                     <div class="movie-poster movie-poster-empty">
-                                        <h5>全部</h5>
-                                        <p>{{ hotMovies.total }}部</p>
+                                        <div class="movie-item-total">
+                                            <h5>全部</h5>
+                                            <p>{{ hotMovies.total }}部</p>
+                                        </div>
                                     </div>
-                                    <p>-</p>
                                 </a>
                             </li>
                         </ul>
@@ -51,18 +58,17 @@
                             <li v-for="(item, index) in soonMovies.subjects" v-if="index < 10" class="movie-item">
                                 <a href="javascript:void(0);">
                                     <div class="movie-poster" :style="'background-image:url(' + item.images.large+ ')'"></div>
-                                    <p>{{ item.title }}</p>
-                                    <p>{{item.wish_count}}人想看</p>
+                                    <p class="movie-item-name">{{ item.title }}</p>
                                 </a>
                             </li>
                             <li class="movie-item">
                                 <a href="javascript:void(0);">
                                     <div class="movie-poster movie-poster-empty">
-                                        <h5>全部</h5>
-                                        <p>{{ soonMovies.total }}部</p>
+                                        <div class="movie-item-total">
+                                            <h5>全部</h5>
+                                            <p>{{ soonMovies.total }}部</p>
+                                        </div>
                                     </div>
-                                    <p>-</p>
-                                    <p>-</p>
                                 </a>
                             </li>
                         </ul>
@@ -81,11 +87,11 @@
                                 <p>8分以上好电影</p>
                                 <div class="movies-rank-posters movie-item">
                                     <div class="movie-poster movie-poster-1"
-                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2421855655.jpg);"></div>
+                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p1910813120.jpg);"></div>
                                     <div class="movie-poster movie-poster-2"
-                                         style="background-image: url(https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2395733377.jpg);"></div>
+                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p511118051.jpg);"></div>
                                     <div class="movie-poster movie-poster-3"
-                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2361744534.jpg);"></div>
+                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p480747492.jpg);"></div>
                                 </div>
                             </a>
                             <a class="movies-rank-item movies-rank-item-week" href="javascript: void(0);">
@@ -93,11 +99,11 @@
                                 <p>3月10日 - 3月17日</p>
                                 <div class="movies-rank-posters movie-item">
                                     <div class="movie-poster movie-poster-1"
-                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2421855655.jpg);"></div>
+                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2362603905.jpg);"></div>
                                     <div class="movie-poster movie-poster-2"
-                                         style="background-image: url(https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2395733377.jpg);"></div>
+                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2417948644.jpg);"></div>
                                     <div class="movie-poster movie-poster-3"
-                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2361744534.jpg);"></div>
+                                         style="background-image: url(https://img5.doubanio.com/view/movie_poster_cover/lpst/public/p2356912156.jpg);"></div>
                                 </div>
                             </a>
                             <a class="movies-rank-item movies-rank-item-new" href="javascript: void(0);">
@@ -105,11 +111,11 @@
                                 <p>3月10日 - 3月17日</p>
                                 <div class="movies-rank-posters movie-item">
                                     <div class="movie-poster movie-poster-1"
-                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2421855655.jpg);"></div>
+                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2408394301.jpg);"></div>
                                     <div class="movie-poster movie-poster-2"
-                                         style="background-image: url(https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2395733377.jpg);"></div>
+                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2421855655.jpg);"></div>
                                     <div class="movie-poster movie-poster-3"
-                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2361744534.jpg);"></div>
+                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2412370372.jpg);"></div>
                                 </div>
                             </a>
                             <a class="movies-rank-item movies-rank-item-top" href="javascript: void(0);">
@@ -117,11 +123,11 @@
                                 <p>票房最高排名</p>
                                 <div class="movies-rank-posters movie-item">
                                     <div class="movie-poster movie-poster-1"
-                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2421855655.jpg);"></div>
+                                         style="background-image: url(https://img1.doubanio.com/view/movie_poster_cover/mpst/public/p2432493858.jpg);"></div>
                                     <div class="movie-poster movie-poster-2"
-                                         style="background-image: url(https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2395733377.jpg);"></div>
+                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/mpst/public/p2429713841.jpg);"></div>
                                     <div class="movie-poster movie-poster-3"
-                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2361744534.jpg);"></div>
+                                         style="background-image: url(https://img3.doubanio.com/view/movie_poster_cover/mpst/public/p2431980130.jpg);"></div>
                                 </div>
                             </a>
                         </div>
@@ -167,23 +173,25 @@
 	}
 </script>
 <style lang="less">
-    section .section-body{
+    .tab-movies .section-body{
         padding-right: 0;
-        padding-bottom: 10px;
         overflow-y: hidden;
         .h-scroll-box{
-            margin-bottom: -40px;
+            margin-bottom: -30px;
             overflow: hidden;
         }
     }
+
     .movie-item{
         display: inline-block;
         width: 100px;
         margin-right: 10px;
+        vertical-align: top;
         &:last-child{
              margin-right: 15px;
          }
         .movie-poster {
+            position: relative;
             width: 100%;
             overflow: hidden;
             background-size: cover;
@@ -194,10 +202,25 @@
                  float: left;
                  margin-top: 142%;
              }
+            .movie-item-total{
+                position: absolute;
+                width: 30px;
+                margin-left: -15px;
+                margin-top: -20px;
+                left: 50%;
+                top: 50%;
+                text-align: center;
+                color: #a7a7a7;
+                p{
+                    font-size: 12px;
+                    border-top: 1px solid #a7a7a7;
+                    transform: scale(0.8);
+                }
+            }
         }
-        p{
+        p.movie-item-name{
+            margin-top: 4px;
             font-size: 12px;
-            line-height: 2;
             color: #525252;
             text-overflow: ellipsis;
             overflow: hidden;
@@ -216,15 +239,36 @@
                      height: 18px;
                  }
             }
+        }
     }
+    .movie-item-rating {
+        font-size: 12px;
+        line-height: 1.2;
+        color: #e09015;
+        .rating-star-bg {
+            display: inline-block;
+            float: left;
+            height: 11px;
+            width: 55px;
+            background: url("../assets/images/rating_star_large_off.png") repeat-x;
+            background-size: 11px 11px;
+            margin-right: 4px;
+            margin-top: 1px;
+            .rating-star-on {
+                display: block;
+                background: url("../assets/images/rating_star_large_on.png") repeat-x;
+                background-size: 11px 11px;
+                height: 11px;
+            }
+        }
     }
 
 /* ********* */
     .movies-rank-item{
         display: inline-block;
         margin-right: 10px;
-        width: 160px;
-        height: 150px;
+        width: 150px;
+        height: 140px;
         background: linear-gradient(to bottom, #e1a01a, rgba(255, 196, 108, 0.2));
         color: #fff;
         border-radius: 4px;
@@ -238,13 +282,14 @@
          }
         &.movies-rank-item-top{
              background: linear-gradient(to bottom, #e56c87, rgba(229, 108, 135, 0.2));
+             margin-right: 15px;
          }
     }
     .movies-rank-posters.movie-item{
         position: relative;
         display: block;
         margin: 6px auto;
-        width: 140px;
+        width: 130px;
     }
     .movies-rank-item {
         h3 {
@@ -258,13 +303,13 @@
     }
     .movie-poster.movie-poster-3{
         position: absolute;
-        width: 60px;
-        margin-left: -32px;
+        width: 54px;
+        margin-left: -27px;
         left: 50%;
     }
     .movie-poster.movie-poster-2,
     .movie-poster.movie-poster-1{
-        width: 46px;
+        width: 40px;
         margin-top: 10px;
     }
     .movie-poster.movie-poster-2{
